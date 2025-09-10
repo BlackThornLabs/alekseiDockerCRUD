c554f95c7859ddc1845396c553f295c65c2df528
Aleksei Borodulia <a.borodulya@yahoo.com>
2025-09-10 16:07:08 +0300

    - изменения в user-service как DiscoveryClient;

--- user-service/src/main/java/com/github/BlackThornLabs/UserServiceApplication.java
+++ user-service/src/main/java/com/github/BlackThornLabs/UserServiceApplication.java
@@ -2,22 +2,11 @@ package com.github.BlackThornLabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class UserServiceApplication {
     public static void main(String[] args) {
-        // Получаем контекст
-        ConfigurableApplicationContext context = SpringApplication.run(UserServiceApplication.class, args);
-
-        // Получаем бин Console из контекста (если активен профиль "console")
-        try {
-            Console console = context.getBean(Console.class);
-            console.start(args);
-        } catch (Exception e) {
-            // Если бина Console нет, логгируем и даем приложению работать как web-сервер
-            System.out.println("Профиль console не активен. Запускаем веб-приложение...");
-        }
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 }
 
